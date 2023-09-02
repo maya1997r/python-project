39c6603e24557b64e1fc9bda042ae08fb3e3236e
Maya <maya.hamarneh@yahoo.com>
2023-09-02 14:03:23 +0300

    initial grid is created

--- python_project.py
+++ python_project.py
@@ -1,18 +1,36 @@
 import tkinter as tk
 
-def close_window(event):
-    root.destroy()
-
+# Create the main window
 root = tk.Tk()
-root.geometry("400x300")  
-root.configure(bg="black")  
+root.title("tik tak toe")
 
 
-canvas = tk.Canvas(root, bg="black", width=400, height=300)
-canvas.pack()
+#make the grid inside a frame
 
-circle_button = canvas.create_oval(5, 5, 30, 30, fill="red")
+frame = tk.Frame(root, padx=20, pady=20)
+frame.pack()
 
-canvas.tag_bind(circle_button, "<Button-1>", close_window)
+
+
+buttons = [[None, None, None],
+           [None, None, None],
+           [None, None, None]]
+
+def button_click(row, column):
+    buttons[row][column]["text"] = "X"
+
+
+for i in range(3):
+    for j in range(3):
+        buttons[i][j] = tk.Button(frame, text = "0" , command = lambda i=i, j=j: button_click(i, j))
+        buttons[i][j].grid(row=i , column = j)
+
+
+
+
+
+
+
+
 
 root.mainloop()

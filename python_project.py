d730b7e7d3a866ca03796f9661d226f6b6d6faec
Maya <maya.hamarneh@yahoo.com>
2023-09-03 19:54:04 +0300

    added the class

--- python_project.py
+++ python_project.py
@@ -1,86 +1,102 @@
 import tkinter as tk
 from tkinter import messagebox
-# Create the main window
-root = tk.Tk()
-root.title("tik tak toe")
 
 
-#make the grid inside a frame
-
-frame = tk.Frame(root)
-frame.pack(padx=10, pady= 30)
 
 
-buttons = [[None, None, None],
+class Tictactoe:
+    def __init__(self, root):
+          self.root = root
+          self.root.title("Tic Tac Toe")
+          self.a = "X"
+
+          
+
+          self.create()
+          
+          
+
+
+    def create(self):
+        
+        frame = tk.Frame(root)
+        frame.pack()
+        self.buttons = [[None, None, None],
            [None, None, None],
            [None, None, None]]
 
-a = "X"
-
-def reset():
-    for i in range(3):
-        for j in range(3):
-            buttons[i][j] = tk.Button(frame, text = " " , height=10 , width= 20, command = lambda i=i, j=j: button_click(i, j))
-            buttons[i][j].grid(row=i , column = j)
-
-
-def if_full():
-      for i in range(3):
+        for i in range(3):
             for j in range(3):
-                  if buttons[i][j]["text"] == " ":
+                self.buttons[i][j] = tk.Button(frame, text = " " , height=5 , width= 20, command = lambda i=i, j=j: self.button_click(i, j))
+                self.buttons[i][j].grid(row=i , column = j)
+
+
+    def reset(self):
+         for i in range(3):
+            for j in range(3):
+                 self.buttons[i][j]["text"] = " "
+                 self.buttons[i][j].config(bg="white")
+    
+    def check(self, row,column):
+         for i in range(3):
+            # check if vertical
+                if self.buttons[i][0]['text'] == self.buttons[i][1]['text'] == self.buttons[i][2]['text'] != " ":
+                    
+                    self.buttons[i][0].config(bg="red")
+                    self.buttons[i][1].config(bg="red")
+                    self.buttons[i][2].config(bg="red")
+                    messagebox.showinfo(f" {self.a} you won")
+                    self.reset()
+
+                elif self.buttons[0][i]['text'] == self.buttons[1][i]['text'] == self.buttons[2][i]['text'] != " ":
+                    
+                    self.buttons[0][i].config(bg="red")
+                    self.buttons[1][i].config(bg="red")
+                    self.buttons[2][i].config(bg="red")
+                    messagebox.showinfo(f" {self.a} you won")
+                    self.reset()
+
+                elif self.buttons[0][0]['text'] == self.buttons[1][1]['text'] == self.buttons[2][2]['text'] != " ":
+                    self.buttons[0][0].config(bg="red")
+                    self.buttons[1][1].config(bg="red")
+                    self.buttons[2][2].config(bg="red")
+                    messagebox.showinfo(f" {self.a} you won")
+                    
+                    self.reset()
+                elif self.buttons[0][2]['text'] == self.buttons[1][1]['text'] == self.buttons[2][0]['text'] != " ":
+                    
+                    self.buttons[0][2].config(bg="red")
+                    self.buttons[1][1].config(bg="red")
+                    self.buttons[2][0].config(bg="red")
+                    messagebox.showinfo(f" {self.a} you won")
+                    self.reset()
+
+                elif self.if_full() == True:
+                     self.reset()
+    
+    def button_click(self, row, column):
+         if self.buttons[row][column]["text"] == " " and self.a == "X":
+            self.buttons[row][column]["text"] = self.a
+            self.check(row,column)
+            self.a = "Y"
+
+         elif self.buttons[row][column]["text"] == " " and self.a == "Y":
+              self.buttons[row][column]["text"] = self.a
+              self.check(row,column)
+              self.a = "X"
+    
+    def if_full(self):
+         for i in range(3):
+            for j in range(3):
+                  if self.buttons[i][j]["text"] == " ":
                         return False
                   
-      return True
-                  
-            
+         return True
+    
+    
 
-     
-      
+if __name__ == "__main__":
+    root = tk.Tk()
+    game = Tictactoe(root)
 
-
-
-
-
-def check(row, column):
-        
-        
-
-        
-            for i in range(3):
-            # check if vertical
-                if buttons[i][0]['text'] == buttons[i][1]['text'] == buttons[i][2]['text'] != " ":
-                    messagebox.showinfo(f" {a} you won")
-                    reset()
-
-                elif buttons[0][i]['text'] == buttons[1][i]['text'] == buttons[2][i]['text'] != " ":
-                    messagebox.showinfo(f" {a} you won")
-                    reset()
-
-                elif buttons[0][0]['text'] == buttons[1][1]['text'] == buttons[2][2]['text'] != " ":
-                    messagebox.showinfo(f" {a} you won")
-                    reset()
-                elif buttons[0][2]['text'] == buttons[1][1]['text'] == buttons[2][0]['text'] != " ":
-                    messagebox.showinfo(f" {a} you won")
-                    reset()
-
-                elif if_full() == True:
-                     reset()
-
-                
-def button_click(row, column):   
-        global a
-        if buttons[row][column]["text"] == " " and a == "X":
-            buttons[row][column]["text"] = a
-            check(row,column)
-            a = "Y"
-
-        elif buttons[row][column]["text"] == " " and a == "Y":
-            buttons[row][column]["text"] = a
-            check(row,column)
-            a = "X"
-
-           
-
-
-reset()
-root.mainloop()
+    root.mainloop()

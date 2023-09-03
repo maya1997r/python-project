680f8afe38cd9c9d4287e30cab778b7fc0cca62e
Maya <maya.hamarneh@yahoo.com>
2023-09-03 13:35:56 +0300

    fixed some ugliness

--- python_project.py
+++ python_project.py
@@ -8,7 +8,7 @@ root.title("tik tak toe")
 #make the grid inside a frame
 
 frame = tk.Frame(root)
-frame.pack()
+frame.pack(padx=10, pady= 30)
 
 
 buttons = [[None, None, None],
@@ -20,7 +20,7 @@ a = "X"
 def reset():
     for i in range(3):
         for j in range(3):
-            buttons[i][j] = tk.Button(frame, text = " " , command = lambda i=i, j=j: button_click(i, j))
+            buttons[i][j] = tk.Button(frame, text = " " , height=10 , width= 20, command = lambda i=i, j=j: button_click(i, j))
             buttons[i][j].grid(row=i , column = j)
 
 
@@ -49,18 +49,18 @@ def check(row, column):
             for i in range(3):
             # check if vertical
                 if buttons[i][0]['text'] == buttons[i][1]['text'] == buttons[i][2]['text'] != " ":
-                    messagebox.showinfo(f"{a}you won")
+                    messagebox.showinfo(f" {a} you won")
                     reset()
 
                 elif buttons[0][i]['text'] == buttons[1][i]['text'] == buttons[2][i]['text'] != " ":
-                    messagebox.showinfo(f"{a}you won")
+                    messagebox.showinfo(f" {a} you won")
                     reset()
 
                 elif buttons[0][0]['text'] == buttons[1][1]['text'] == buttons[2][2]['text'] != " ":
-                    messagebox.showinfo(f"{a}you won")
+                    messagebox.showinfo(f" {a} you won")
                     reset()
                 elif buttons[0][2]['text'] == buttons[1][1]['text'] == buttons[2][0]['text'] != " ":
-                    messagebox.showinfo(f"{a}you won")
+                    messagebox.showinfo(f" {a} you won")
                     reset()
 
                 elif if_full() == True:

e070cf152a9794cc4c6ffc065b6bac13cd986e5e
Maya <maya.hamarneh@yahoo.com>
2023-09-05 00:19:42 +0300

    still having problem with restarting all of them

--- python_project.py
+++ python_project.py
@@ -1,34 +1,34 @@
 import tkinter as tk
 from tkinter import messagebox
 
-
-
+frame_width = 90
+frame_height = 45
 
 class Tictactoe:
-    def __init__(self, root):
-          self.root = root
-          self.root.title("Tic Tac Toe")
-          self.a = "X"
-
-          
-
-          self.create()
-          
-          
-
-
-    def create(self):
+    def __init__(self, frame):
         
-        frame = tk.Frame(root)
-        frame.pack()
-        self.buttons = [[None, None, None],
-           [None, None, None],
-           [None, None, None]]
+        
+        #self.root.geometry(f"{window_width}x{window_height}")
+        self.current_player = "X"
+
+        
+        self.state = [[0, 0, 0],
+           [0, 0, 0],
+           [0, 0, 0]]
+        
+        self.buttons = []
+
 
         for i in range(3):
+            self.buttons.append([])
             for j in range(3):
-                self.buttons[i][j] = tk.Button(frame, text = " " , height=5 , width= 20, command = lambda i=i, j=j: self.button_click(i, j))
-                self.buttons[i][j].grid(row=i , column = j)
+                button = tk.Button(frame, text = " " , height= int((1/9) * frame_height), width= int((1/9) * frame_width), command = lambda i=i, j=j: self.button_click(i, j))
+                button.grid(row=i , column = j)
+                self.buttons[i].append(button)
+
+          
+
+          
 
 
     def reset(self):
@@ -36,59 +36,63 @@ class Tictactoe:
             for j in range(3):
                  self.buttons[i][j]["text"] = " "
                  self.buttons[i][j].config(bg="white")
+                 self.state[i][j] = 0
     
     def check(self, row,column):
          for i in range(3):
             # check if vertical
-                if self.buttons[i][0]['text'] == self.buttons[i][1]['text'] == self.buttons[i][2]['text'] != " ":
+                if (self.state[i][0] == self.state[i][1] == self.state[i][2] == 1) or (self.state[i][0] == self.state[i][1] == self.state[i][2] == 2):
                     
                     self.buttons[i][0].config(bg="red")
                     self.buttons[i][1].config(bg="red")
                     self.buttons[i][2].config(bg="red")
-                    messagebox.showinfo(f" {self.a} you won")
+                    messagebox.showinfo(f" {self.current_player} you won")
                     self.reset()
 
-                elif self.buttons[0][i]['text'] == self.buttons[1][i]['text'] == self.buttons[2][i]['text'] != " ":
+                elif (self.state[0][i] == self.state[1][i] == self.state[2][i] == 1) or (self.state[0][i] == self.state[1][i] == self.state[2][i] == 2):
                     
                     self.buttons[0][i].config(bg="red")
                     self.buttons[1][i].config(bg="red")
                     self.buttons[2][i].config(bg="red")
-                    messagebox.showinfo(f" {self.a} you won")
+                    messagebox.showinfo(f" {self.current_player} you won")
                     self.reset()
 
-                elif self.buttons[0][0]['text'] == self.buttons[1][1]['text'] == self.buttons[2][2]['text'] != " ":
+                elif (self.state[0][0] == self.state[1][1] == self.state[2][2] == 1) or (self.state[0][0] == self.state[1][1] == self.state[2][2] == 2):
                     self.buttons[0][0].config(bg="red")
                     self.buttons[1][1].config(bg="red")
                     self.buttons[2][2].config(bg="red")
-                    messagebox.showinfo(f" {self.a} you won")
+                    messagebox.showinfo(f" {self.current_player} you won")
                     
                     self.reset()
-                elif self.buttons[0][2]['text'] == self.buttons[1][1]['text'] == self.buttons[2][0]['text'] != " ":
+                elif (self.state[0][2] == self.state[1][1] == self.state[2][0] == 1) or (self.state[0][2] == self.state[1][1] == self.state[2][0] == 2):
                     
                     self.buttons[0][2].config(bg="red")
                     self.buttons[1][1].config(bg="red")
                     self.buttons[2][0].config(bg="red")
-                    messagebox.showinfo(f" {self.a} you won")
+                    messagebox.showinfo(f" {self.current_player} you won")
                     self.reset()
 
                 elif self.if_full() == True:
                      self.reset()
     
     def button_click(self, row, column):
-         if self.buttons[row][column]["text"] == " " and self.a == "X":
-            self.buttons[row][column]["text"] = self.a
+         if self.state[row][column] == 0 and self.current_player == "X":
+            self.buttons[row][column]["text"] = self.current_player
+            self.state[row][column] = 1
             self.check(row,column)
-            self.a = "Y"
+            self.current_player = "Y"
 
-         elif self.buttons[row][column]["text"] == " " and self.a == "Y":
-              self.buttons[row][column]["text"] = self.a
+         elif self.state[row][column] == 0 and self.current_player == "Y":
+              self.buttons[row][column]["text"] = self.current_player
+              self.state[row][column] = 2
               self.check(row,column)
-              self.a = "X"
+              self.current_player = "X"
     
     def if_full(self):
+         #all(self.state, lambda row: all(row, lambda x: x!= 0))
          for i in range(3):
             for j in range(3):
-                  if self.buttons[i][j]["text"] == " ":
+                  if self.state[i][j] == 0:
                         return False
                   
          return True
@@ -96,7 +100,30 @@ class Tictactoe:
     
 
 if __name__ == "__main__":
+
+
     root = tk.Tk()
-    game = Tictactoe(root)
+    root.title("Tic Tac Toe")
+
+    frames = []
+
+    for i in range(3):
+        row_frames = []
+        for j in range(3):
+            frame = tk.Frame(root)
+            frame.grid(row=i, column=j)
+            row_frames.append(frame)
+        frames.append(row_frames)
+
+    games = []
+    for i in range(3):
+        row_games = []
+        for j in range(3):
+            game = Tictactoe(frames[i][j])
+            row_games.append(game)
+        games.append(row_games)
+
+    restart_button = tk.Button(root, text="restart game", command=lambda: [game.reset() for row in games])
+    restart_button.grid()
 
     root.mainloop()
